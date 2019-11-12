eed08cfbfec5326a62b939905af68e939dfbcbea
Salem <mymainemail0501@gmail.com>
2019-11-12 14:44:31 -0600

    favicon

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,7 @@ class Header extends React.Component {
         return (
         <>
             <Helmet htmlAttributes={{ lang: 'en' }}>
-            <title>Title</title>
+            <title>LianAriel</title>
             <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" data-api-key="ZjVkN2M3YzEtOGU0Yy00ZTUzLWFiZGQtNTNkZjY3NGM2YThkNjM3MDg5MjIyODA1Nzg2OTk5" id="snipcart"></script>
               <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
               <link href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css" rel="stylesheet" type="text/css" />
